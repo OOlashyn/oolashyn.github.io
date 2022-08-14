cf3879ed1d5df0df09d4388d3bfce698035e3a51
Oleksandr Olashyn (dancingwithcrm) <aleksanderolashyn@gmail.com>
2022-08-13 22:14:56 -0400

    Update copy code to use Clipboard API

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,26 +1,13 @@
-const copyCode = copyCodeButton => {
-    const tempTextArea = document.createElement("textarea");
-    tempTextArea.textContent = copyCodeButton.getAttribute("data-code");
-    document.body.appendChild(tempTextArea);
+const copyCode = async (copyCodeButton) => {
+    const code = copyCodeButton.getAttribute("data-code");
 
-    const selection = document.getSelection();
-    selection.removeAllRanges();
-    tempTextArea.select();
-    document.execCommand("copy");
-    selection.removeAllRanges();
-    document.body.removeChild(tempTextArea);
+    await navigator.clipboard.writeText(code);
 
     copyCodeButton.classList.add("copied");
     setTimeout(() => {
         copyCodeButton.classList.remove("copied");
     }, 2000);
 };
-
-document.addEventListener("keyup", keyEvent => {
-    if (keyEvent.keyCode === 13) {
-        copyCode(keyEvent.target);
-    }
-});
 
 document.querySelectorAll(".copy-code-button").forEach(copyCodeButton => {
     copyCodeButton.addEventListener("click", clickEvent =>
