2a142691f62f3a88d93cbc1724e7fa311d423fe3
Oleksandr Olashyn (dancingwithcrm) <oleksandr@dancingwithcrm.com>
2026-04-09 20:43:49 -0400

    Updated the URL generation logic and file naming to be the same as kramdown

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@
  * Strips the file extension (.mdx, .md) from the ID.
  */
 export function getPostSlug(postId: string): string {
-  return postId.replace(/\.mdx?$/, '');
+  return postId.replace(/\.mdx?$/, '').replace(/^\d{4}-\d{2}-\d{2}-/, '');
 }
 
 /**
